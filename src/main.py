3281cd27ac4cd95c4ed6b9290cc6afede78dde75
Israel Arantes <israel.arantes.moraes@gmail.com>
2025-01-28 22:22:32 -0300

    Triyng github message

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from textnode import TextNode, TextType
 def main():
     test = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(test)
-    
+    print("test case of not contributions to github")
 if __name__=="__main__":
     main()
