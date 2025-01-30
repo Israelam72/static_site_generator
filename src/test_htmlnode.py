dfb60ccbb747287adaa999c4eb7d54209d27a71b
Israelam72 <israelarantes.m@gmail.com>
2025-01-30 19:17:59 -0300

    new test cases for htmlnode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,16 +2,25 @@ import unittest
 from htmlnode import HTLMNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_nodesprops(self):
-        test = {
+    test = {
             "href": "https://www.google.com",
             "target": "_blank",
             "target2": "_blank2",
         }
-        strg = ' href="https://www.google.com" target="_blank" target2="_blank2"'
-        node = HTLMNode("<p>", "Israel", props=test)
-        node2 = HTLMNode("<a>", "Israel2", props=test)
-        self.assertEqual(node.props_to_html(), strg)
+    strg = ' href="https://www.google.com" target="_blank" target2="_blank2"'
+
+    def test_nodesprops(self):
+        node = HTLMNode("<p>", "Israel", props=TestHTMLNode.test)
+        self.assertEqual(node.props_to_html(), TestHTMLNode.strg)
+
+    def test_emptyprops(self):
+        node = HTLMNode("<p>", "Israel", props={})
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_notequal(self):
+        node = HTLMNode("<p>", "Israel", props=TestHTMLNode.test)
+        self.assertNotEqual(node.tag, node.value)
+
 
 if __name__ == "__main__":
     unittest.main()
